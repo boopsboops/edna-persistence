d6c399ec522c13bb890d279a91ce579fcd059109
rupert@jau <rupertcollins@gmail.com>
2018-09-20 14:11:23 +0100

    removed xtable clean

--- code/xtable_clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sed -i -e 's/crab-hex/Crab/g' -e 's/shanny-fam/Shanny/g' -e 's/summer/Summer/g' -e 's/winter/Winter/g' -e 's/e1-full/Offshore/g' -e 's/e1-twothirds/Offshore two-thirds/g' -e 's/harbour-twothirds/Inshore two-thirds/g' -e 's/harbour-full/Inshore/g' -e 's/-/$-$/g' xtab
